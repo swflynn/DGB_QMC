963a67c61da0283b4ed31092806c450257955d31
swflynn <swflynn@uci.edu>
2019-04-04 10:53:26 -0700

    Submission Scripts

--- scripts/cluster/Generate/runner.sh
+++ scripts/cluster/Generate/runner.sh
@@ -1,3 +1,15 @@
+#=============================================================================80
+#                           Generate Input Files 
+#==============================================================================!
+#    Discussion:
+#Search all subdirectories, if they contain the cluster submission (submit.sh) 
+#file then move to that directory and submit (qsub)
+#==============================================================================!
+#   Modified:
+# 4 April 2019
+#   Author:
+# Shane Flynn
+#==============================================================================!
 for d in `find . -type d`
 do ( cd "$d"
      if test ! -f submit.sh; then continue; fi
